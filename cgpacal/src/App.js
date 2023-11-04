25025d7f7562bd9439cb5d892311c8fcae1e353c
poswalsameer <poswalsameer2003@gmail.com>
2023-11-04 19:37:53 +0530

    Setup of the project with adding Tailwind CSS and initial elements of the app

---
+++ cgpacal/src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className=" flex flex-col justify-center items-center content-evenly " >
+        <h1 className=" text-pink-300 text-center m-10 text-8xl " >CGPA CALCULATOR</h1>
+        <div className=" h-3/4 w-3/4 bg-blue-200 text-black m-6 p-7 rounded-xl flex flex-col justify-center items-center " > 
+
+          <p className=" font-bold text-4xl m-4 " >SELECT YOUR BRANCH </p>
+
+          <div>
+
+              
+
+          </div>          
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
